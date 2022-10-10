2578992bdfebcdb0a25a074a7a9476565ade62aa
AmigoDoCarlos <axelpinheiro@gmail.com>
2022-10-10 10:25:53 -0300

    incluído o ícone da biblioteca feather em um dos botões da tela Home

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -3,8 +3,12 @@ import { gameCards } from './GameCards';
 import ImageSlider from './ImageSlider';
 import './Home.css';
 import Background from '../../Components/Background';
+import { ArrowRight } from 'react-feather';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <Background>
       <div className="HomeHeaderDiv">
@@ -23,9 +27,13 @@ function Home() {
           className="JoinRoomEnterCode"
           placeholder="Digite o código da sala"
         />
-        <Link to="/JoinRoom">
-          <button className="JoinRoomButton" />
-        </Link>
+        <button className="JoinRoomButton">
+          <ArrowRight
+            width="30px"
+            height="30px"
+            onClick={() => navigate('/JoinRoom')}
+          />
+        </button>
       </div>
 
       <div className="CreateRoomDiv">
