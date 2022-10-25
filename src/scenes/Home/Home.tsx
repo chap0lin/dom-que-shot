d068a924c9aed220d161537236ba0cb6c9ed03ee
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-10-25 16:08:05 -0300

    Added room code validation

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -6,6 +6,7 @@ import ImageSlider from './ImageSlider';
 import Background from '../../components/Background';
 import Header from '../../components/Header';
 import Button from '../../components/Button';
+import api from '../../services/api';
 import './Home.css';
 
 function Home() {
@@ -28,88 +29,94 @@ function Home() {
   };
 
   const verifyRoom = () => {
-    if (roomCode.length > 3) {
-      console.log('Sala inserida: ' + roomCode);
-      navigate('/ChooseAvatar', { state: { option: 'join' } });
-      return;
-    }
-    if (roomCode.length > 0) {
+
+    if (roomCode.length == 6) {
+      api.get(`/roomCode/${roomCode}`).then((response) => {
+        console.log(response.data);
+        navigate('/ChooseAvatar', {
+          state: { option: 'join', roomCode: roomCode },
+        });
+      }).catch((error) => {
+        setInputErrorMsg({
+          msg: "Sala inexistente! Tente novamente",
+          display: 'block',
+        });
+      });
+    } else {
       setInputErrorMsg({
-        msg: 'Código inválido! Tente novamente!',
+        msg: 'Código inválido! Tente novamente',
         display: 'block',
       });
-      return;
     }
-    setInputErrorMsg({ msg: 'Nenhum código inserido.', display: 'block' });
-  };
+  }
 
-  ////Listener para remover foco do <input> quando o usuário aperta Enter/////////////////////////
+    ////Listener para remover foco do <input> quando o usuário aperta Enter/////////////////////////
 
-  const ref = useRef(null);
+    const ref = useRef(null);
 
-  useEffect(() => {
-    document.addEventListener('keydown', detectKeyDown);
-    return () => {
-      document.removeEventListener('keydown', detectKeyDown);
+    useEffect(() => {
+      document.addEventListener('keydown', detectKeyDown);
+      return () => {
+        document.removeEventListener('keydown', detectKeyDown);
+      };
+    }, []);
+
+    const detectKeyDown = (e) => {
+      if (e.key === 'Enter') {
+        ref.current.blur();
+      }
     };
-  }, []);
 
-  const detectKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      ref.current.blur();
-    }
-  };
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+    return (
+      <Background>
+        <Header title="Vamos começar?" />
 
-  return (
-    <Background>
-      <Header title="Vamos começar?" />
-
-      <div className="JoinRoomDiv">
-        <div className="JoinRoomWarningSpace">
-          <AlertTriangle
-            width="20px"
-            height="20px"
-            color="red"
-            style={{ display: inputErrorMsg.display }}
-          />
-          <p
-            style={{ display: inputErrorMsg.display }}
-            className="JoinRoomWarning">
-            {inputErrorMsg.msg}
-          </p>
-        </div>
-        <div className="JoinRoomInputAndButton">
-          <input
-            ref={ref}
-            onChange={updateRoomCode}
-            className="JoinRoomEnterCode"
-            placeholder="Digite o código da sala"
-          />
-          <button className="JoinRoomButton">
-            <ArrowRight width="30px" height="30px" onClick={verifyRoom} />
-          </button>
-        </div>
-      </div>
-
-      <div className="CreateRoomDiv">
-        <Button width="100%">
-          <div
-            onClick={() =>
-              navigate('/ChooseAvatar', { state: { option: 'create' } })
-            }>
-            Criar Sala
+        <div className="JoinRoomDiv">
+          <div className="JoinRoomWarningSpace">
+            <AlertTriangle
+              width="20px"
+              height="20px"
+              color="red"
+              style={{ display: inputErrorMsg.display }}
+            />
+            <p
+              style={{ display: inputErrorMsg.display }}
+              className="JoinRoomWarning">
+              {inputErrorMsg.msg}
+            </p>
           </div>
-        </Button>
-      </div>
+          <div className="JoinRoomInputAndButton">
+            <input
+              ref={ref}
+              onChange={updateRoomCode}
+              className="JoinRoomEnterCode"
+              placeholder="Digite o código da sala"
+            />
+            <button className="JoinRoomButton">
+              <ArrowRight width="30px" height="30px" onClick={verifyRoom} />
+            </button>
+          </div>
+        </div>
 
-      <div className="ChooseGameDiv">
-        <p>Já sabe o que quer?</p>
-        <ImageSlider content={gameCards} />
-      </div>
-    </Background>
-  );
-}
+        <div className="CreateRoomDiv">
+          <Button width="100%">
+            <div
+              onClick={() =>
+                navigate('/ChooseAvatar', { state: { option: 'create' } })
+              }>
+              Criar Sala
+            </div>
+          </Button>
+        </div>
 
-export default Home;
+        <div className="ChooseGameDiv">
+          <p>Já sabe o que quer?</p>
+          <ImageSlider content={gameCards} />
+        </div>
+      </Background>
+    );
+  }
+
+  export default Home
