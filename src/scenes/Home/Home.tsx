6e754af8dfb948812023815b9525e5b93c11c931
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-10-25 16:08:05 -0300

    Added room code validation

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -6,6 +6,7 @@ import ImageSlider from './ImageSlider';
 import Background from '../../components/Background';
 import Header from '../../components/Header';
 import Button from '../../components/Button';
+import api from '../../services/api';
 import './Home.css';
 
 function Home() {
@@ -27,22 +28,28 @@ function Home() {
   };
 
   const verifyRoom = () => {
-    if (roomCode.length > 3) {
-      console.log('Entrando na sala ' + roomCode + '.');
-      navigate('/ChooseAvatar', {
-        state: { option: 'join', roomCode: roomCode },
-      });
-      return;
-    }
+
     if (roomCode.length > 0) {
+      api.get(`/roomCode/${roomCode}`).then((response) => {
+        console.log(response.data);
+        navigate('/ChooseAvatar', {
+          state: { option: 'join', roomCode: roomCode },
+        });
+        return;
+      }).catch((error) => {
+        setInputErrorMsg({
+          msg: "Código inválido! Tente novamente",
+          visibility: 'visible',
+        });
+        return;
+      });
+    } else {
       setInputErrorMsg({
-        msg: 'Código inválido! Tente novamente!',
+        msg: 'Nenhum código inserido.',
         visibility: 'visible',
       });
-      return;
     }
-    setInputErrorMsg({ msg: 'Nenhum código inserido.', visibility: 'visible' });
-  };
+  }
 
   ////Listener para remover foco do <input> quando o usuário aperta Enter/////////////////////////
 
